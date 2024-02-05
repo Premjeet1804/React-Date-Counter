893c244cdb8af744cac1a23a2df4307579f1c4fb
Premjeet Sahu <cs2054@global.org.in>
2024-02-05 18:25:54 +0530

    new version

--- src/App.js
+++ src/App.js
@@ -21,17 +21,31 @@ function Counter() {
   const [step, setStep] = useState(1);
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
+  function setState() {
+    setCount(0);
+    setStep(1);
+  }
   return (
     <div className="btn">
       <div>
-        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+
         <span> Step : {step} </span>
-        <button onClick={() => setStep((s) => s + 1)}>+</button>
       </div>
 
       <div>
         <button onClick={() => setCount((c) => c - step)}>-</button>
-        <span> Count : {count} </span>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
         <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
       <p>
@@ -44,6 +58,11 @@ function Counter() {
           {date.toDateString()}
         </span>
       </p>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={setState}>reset</button>
+        </div>
+      ) : null}
     </div>
   );
 }
